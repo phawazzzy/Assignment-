989126835f53eedcb612fab8734bf5e31eaddd5e
fawas kareem <phawazzzy@gmail.com>
2018-08-14 02:06:27 +0100

    form with hide/show button

---
+++ form.js
@@ -0,0 +1,13 @@
+let hidebutton = document.getElementById("hidebutton");
+let showbutton = document.getElementById("showbutton");
+
+hidebutton.onclick = function(){
+    document.getElementById("form");
+    form.style.display = "none";
+
+}
+
+showbutton.onclick = function(){
+    document.getElementById("form");
+    form.style.display = "none";
+}
